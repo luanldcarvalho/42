6eb97ed71416f7e9d8cafaac35882d71e98d4939
Luan <luanldcarvalho@gmail.com>
2021-08-30 17:47:21 -0300

    resolve #34; lib added

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,9 +6,11 @@
 /*   By: lluciano <lluciano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 13:17:58 by lluciano          #+#    #+#             */
-/*   Updated: 2021/08/30 15:15:17 by lluciano         ###   ########.fr       */
+/*   Updated: 2021/08/30 17:43:02 by lluciano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 size_t	strlen(const char *str)
 {
