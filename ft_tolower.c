5ab471f7c6e4f2e61d1dcd3946c033b86ca35b2d
Luan <luanldcarvalho@gmail.com>
2021-08-30 17:46:24 -0300

    resolve #7; lib added

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,9 +6,11 @@
 /*   By: lluciano <lluciano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/26 15:16:29 by lluciano          #+#    #+#             */
-/*   Updated: 2021/08/26 15:16:49 by lluciano         ###   ########.fr       */
+/*   Updated: 2021/08/30 17:43:06 by lluciano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_tolower(int argument)
 {
