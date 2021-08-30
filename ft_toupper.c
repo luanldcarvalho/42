87399647ee9d0979d351c262fdc34f92edffc58f
Luan <luanldcarvalho@gmail.com>
2021-08-30 17:46:04 -0300

    resolve #6; lib added

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,9 +6,11 @@
 /*   By: lluciano <lluciano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/26 15:08:20 by lluciano          #+#    #+#             */
-/*   Updated: 2021/08/26 15:08:23 by lluciano         ###   ########.fr       */
+/*   Updated: 2021/08/30 17:43:09 by lluciano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_toupper(int argument)
 {
