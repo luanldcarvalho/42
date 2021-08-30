75d83bfe3f557a0aa1be0b74b83ee62787749112
Luan <luanldcarvalho@gmail.com>
2021-08-30 17:39:24 -0300

    resolve #14; ft_memset okay

--- ft_memset.c
+++ ft_memset.c
@@ -6,19 +6,21 @@
 /*   By: lluciano <lluciano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 15:33:48 by lluciano          #+#    #+#             */
-/*   Updated: 2021/08/30 16:26:50 by lluciano         ###   ########.fr       */
+/*   Updated: 2021/08/30 17:38:26 by lluciano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memset(char *str, int c, size_t n)
+void	*ft_memset(void *str1, int c, size_t n)
 {
-	int		index;
+	unsigned int	index;
+	unsigned char	*str2;
 
 	index = 0;
+	str2 = str1;
 	while(index < n)
 	{
-		str[index] = c;
+		str2[index] = (unsigned  char)c;
 		index ++;
 	}
+	return (str1);
 }
-
