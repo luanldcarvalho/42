2dbeea7b526789a1823100067d979a6f3b4299a1
Luan <luanldcarvalho@gmail.com>
2021-08-30 17:41:50 -0300

    resolve #1; lib added

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,9 +6,11 @@
 /*   By: lluciano <lluciano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 16:48:25 by lluciano          #+#    #+#             */
-/*   Updated: 2021/08/25 18:15:57 by lluciano         ###   ########.fr       */
+/*   Updated: 2021/08/30 17:41:17 by lluciano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_isalpha(int argument)
 {
