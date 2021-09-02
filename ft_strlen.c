128804c5d8bf8f4e42bd24a34951947767bc229d
Luan <luanldcarvalho@gmail.com>
2021-09-02 12:34:51 -0300

    resolve #34; Checking and improving the function

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,21 +6,20 @@
 /*   By: lluciano <lluciano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 13:17:58 by lluciano          #+#    #+#             */
-/*   Updated: 2021/09/01 20:43:45 by lluciano         ###   ########.fr       */
+/*   Updated: 2021/09/02 12:32:11 by lluciano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *str)
+size_t	ft_strlen(const char *s)
 {
 	int	index;
 
 	index = 0;
-	while (*str != '\0')
+	while (s[index] != '\0')
 	{
 		index ++;
-		*str ++;
 	}
 	return (index);
 }
--- main/ft_strlen/main.c
+++ main/ft_strlen/main.c
@@ -6,19 +6,27 @@
 /*   By: lluciano <lluciano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 16:48:34 by lluciano          #+#    #+#             */
-/*   Updated: 2021/08/30 17:48:52 by lluciano         ###   ########.fr       */
+/*   Updated: 2021/09/02 12:34:08 by lluciano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <string.h>
+/*size_t strlen(const char *s);*/
 
 int	main(void)
 {
 	char	*teste;
 
 	char	ch[] = {'t', 'e', 's', 't', 'e', '\0'};
-	printf("\nThe string is: %s .Length of string is: %ld\n", ch, strlen(ch));
+	printf("\nUsing [ft_strlen] ===> ");
+	printf("\nThe string is: [%s]. Length of string is: %ld", ch, ft_strlen(ch));
 	teste = "testando";
-	printf("\nThe string is: %s .Length of string is: %ld\n", ch, strlen(teste));
+	printf("\nThe string is: [%s]. Length of string is: %ld", teste, ft_strlen(teste));
+
+	printf("\n\nUsing [strlen] ===> ");
+	printf("\nThe string is: [%s]. Length of string is: %ld", ch, strlen(ch));
+	teste = "testando";
+	printf("\nThe string is: [%s]. Length of string is: %ld\n", teste, strlen(teste));
 	return (0);
 }
