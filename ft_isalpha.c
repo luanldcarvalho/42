17dc6edfc99d70ba47287afcf4e97818b9dbdcc2
Luan <luanldcarvalho@gmail.com>
2021-09-01 21:03:12 -0300

    resolve #1; Checking and improving the function

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,17 +6,17 @@
 /*   By: lluciano <lluciano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 16:48:25 by lluciano          #+#    #+#             */
-/*   Updated: 2021/08/30 17:41:17 by lluciano         ###   ########.fr       */
+/*   Updated: 2021/09/01 20:55:24 by lluciano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isalpha(int argument)
+int	ft_isalpha(int c)
 {
-	if (argument >= 'A' && argument <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	if (argument >= 'a' && argument <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (2);
 	return (0);
 }
--- main/ft_isalpha/main.c
+++ main/ft_isalpha/main.c
@@ -6,19 +6,24 @@
 /*   By: lluciano <lluciano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 13:40:26 by lluciano          #+#    #+#             */
-/*   Updated: 2021/08/30 13:40:27 by lluciano         ###   ########.fr       */
+/*   Updated: 2021/09/01 21:01:25 by lluciano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <ctype.h>
+/*int isalpha(int c);*/
 
 int	main(void)
 {
 	int	res;
+	int	resTest;
 
 	res = ft_isalpha(100);
-	printf("Hello World!");
-	printf("%d", res);
+	printf("Using [ft_isalpha] ===> %d\n", res);
+	resTest = isalpha(100);
+	printf("Using [isalpha] ===> %d\n", res);
 	return (0);
 }
+
 
