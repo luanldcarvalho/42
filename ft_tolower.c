92933992a7e688aa202fd1dfea88a9cd3da4dbaf
Luan <luanldcarvalho@gmail.com>
2021-09-02 12:22:59 -0300

    resolve #7; Checking and improving the function

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,15 +6,15 @@
 /*   By: lluciano <lluciano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/26 15:16:29 by lluciano          #+#    #+#             */
-/*   Updated: 2021/08/30 17:43:06 by lluciano         ###   ########.fr       */
+/*   Updated: 2021/09/02 12:19:29 by lluciano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_tolower(int argument)
+int	ft_tolower(int c)
 {
-	if (argument >= 'A' && argument <= 'Z')
-		return (argument + 32);
+	if (c >= 'A' && c <= 'Z')
+		return (c + 32);
 	return (0);
 }
--- main/ft_tolower/main.c
+++ main/ft_tolower/main.c
@@ -6,17 +6,20 @@
 /*   By: lluciano <lluciano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 13:44:22 by lluciano          #+#    #+#             */
-/*   Updated: 2021/08/30 13:44:24 by lluciano         ###   ########.fr       */
+/*   Updated: 2021/09/02 12:21:56 by lluciano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <ctype.h>
+/*int islower(int c);*/
 
 int	main(void)
 {
 	int	argument;
 	int	result;
 
+	printf("\nUsing [ft_tolower] ===> ");
 	argument = '5';
 	result = ft_tolower(argument);
 	if (result == 0)
@@ -41,5 +44,31 @@ int	main(void)
 		printf ("\n%d (%c) is not a upper case", argument, argument);
 	else
 		printf ("\n%d (%c) after to lower is >> %c", argument, argument, result);
+
+	printf("\n\nUsing [tolower] ===> ");
+	argument = '5';
+	result = tolower(argument);
+	if (result == 0)
+		printf ("\n%d (%c) is not a upper case", argument, argument);
+	else
+		printf ("\n%d (%c) after to lower is >> %c", argument, argument, result);
+	argument = 'a';
+	result = tolower(argument);
+	if (result == 0)
+		printf ("\n%d (%c) is not a upper case", argument, argument);
+	else
+		printf ("\n%d (%c) after to lower is >> %c", argument, argument, result);
+	argument = 'B';
+	result = tolower(argument);
+	if (result == 0)
+		printf ("\n%d (%c) is not a upper case", argument, argument);
+	else
+		printf ("\n%d (%c) after to lower is >> %c", argument, argument, result);
+	argument = 0x800;
+	result = tolower(argument);
+	if (result == 0)
+		printf ("\n%d (%c) is not a upper case\n", argument, argument);
+	else
+		printf ("\n%d (%c) after to lower is >> %c\n", argument, argument, result);
 	return (0);
 }
