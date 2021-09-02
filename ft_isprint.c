a431c2c007a52923b4808f4cc0dd5a9af0bc8156
Luan <luanldcarvalho@gmail.com>
2021-09-02 12:03:16 -0300

    resolve #5; Checking and improving the function

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,15 +6,15 @@
 /*   By: lluciano <lluciano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/26 14:48:18 by lluciano          #+#    #+#             */
-/*   Updated: 2021/08/30 17:42:54 by lluciano         ###   ########.fr       */
+/*   Updated: 2021/09/02 11:58:55 by lluciano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isprint(int argument)
+int	ft_isprint(int c)
 {
-	if (argument >= 33 && argument <= 254)
+	if (c >= 33 && c <= 254)
 		return (1);
 	return (0);
 }
--- main/ft_isprint/main.c
+++ main/ft_isprint/main.c
@@ -6,16 +6,19 @@
 /*   By: lluciano <lluciano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 13:43:17 by lluciano          #+#    #+#             */
-/*   Updated: 2021/08/30 13:43:23 by lluciano         ###   ########.fr       */
+/*   Updated: 2021/09/02 12:01:58 by lluciano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <ctype.h>
+/*int isprint(int c);*/
 
 int	main(void)
 {
 	int	argument;
 
+	printf("\nUsing [ft_isprint] ===>");
 	argument = '5';
 	if (ft_isprint(argument) == 0)
 		printf ("\n%d is not a print value", argument);
@@ -26,5 +29,17 @@ int	main(void)
 		printf ("\n%d is not a print value", argument);
 	else
 		printf ("\n%d is a print value", argument);
+
+	printf("\n\nUsing [isprint] ===>");
+	argument = '5';
+	if (isprint(argument) == 0)
+		printf ("\n%d is not a print value", argument);
+	else
+		printf ("\n%d is a print value", argument);
+	argument = 0x800;
+	if (isprint(argument) == 0)
+		printf ("\n%d is not a print value\n", argument);
+	else
+		printf ("\n%d is a print value\n", argument);
 	return (0);
 }
