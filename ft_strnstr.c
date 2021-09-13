ec21c80df8c8903ff51abbf2c46c691fbeb978e1
Luan <luanldcarvalho@gmail.com>
2021-09-13 17:26:15 -0300

    resolve #13; Improving ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: lluciano <lluciano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 16:02:06 by lluciano          #+#    #+#             */
-/*   Updated: 2021/09/13 17:08:04 by lluciano         ###   ########.fr       */
+/*   Updated: 2021/09/13 17:25:11 by lluciano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,9 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
 	size_t	j;
-	int		length;
 
 	i = 0;
-	length = ft_strlen(little);
-	if (length == 0)
+	if (ft_strlen(little) == 0)
 		return ((char *)big);
 	while (big[i] != 0 && len > i)
 	{
