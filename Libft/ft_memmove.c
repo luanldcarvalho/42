4a9a03b759f1b3c4ce5173627926006dab6dd3db
Luan <luanldcarvalho@gmail.com>
2021-09-25 17:40:59 -0300

    resolve #17; Correction funcion

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -6,23 +6,33 @@
 /*   By: lluciano <lluciano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 13:56:58 by lluciano          #+#    #+#             */
-/*   Updated: 2021/09/13 18:02:39 by lluciano         ###   ########.fr       */
+/*   Updated: 2021/09/25 16:14:28 by lluciano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static void	*memcpyend(void *dest, const void *src, int n)
+{
+	unsigned char	*destine;
+	unsigned char	*source;
+
+	destine = (unsigned char *)dest;
+	source = (unsigned char *)src;
+	while (n--)
+		destine[n] = source[n];
+	return (dest);
+}
+
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char		*destine;
-	const char	*source;
-	char		*memory;
-
-	memory = malloc(sizeof (n));
-	destine = dest;
-	source = src;
-	ft_memcpy(memory, source, n);
-	ft_memcpy(destine, memory, n);
-	free (memory);
+	if (n == 0)
+		return (dest);
+	if (dest == NULL && src == NULL)
+		return (NULL);
+	if (src < dest)
+		memcpyend(dest, src, n);
+	else
+		ft_memcpy(dest, src, n);
 	return (dest);
 }
