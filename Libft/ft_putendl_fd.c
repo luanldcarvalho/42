076f3f10c4a1576510036fb288c3ab52ced18ebc
Luan <luanldcarvalho@gmail.com>
2021-09-27 10:45:24 -0300

    resolve #32; Function ft_putendl_fd.c is Okay

--- Libft/ft_putendl_fd.c
+++ Libft/ft_putendl_fd.c
@@ -1,1 +1,19 @@
-void	ft_putendl_fd(char *s, int fd);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lluciano <lluciano@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/27 10:43:36 by lluciano          #+#    #+#             */
+/*   Updated: 2021/09/27 10:43:53 by lluciano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
+}
--- Libft/libft.h
+++ Libft/libft.h
@@ -36,5 +36,6 @@ char	*ft_strtrim(char const *s1, char const *set);
 //char	*ft_itoa(int n);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 
 #endif
