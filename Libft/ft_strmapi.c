544566634cf6aaa3c1eb3426ed1f739cb3d7891a
Luan <luanldcarvalho@gmail.com>
2021-09-29 15:26:27 -0300

    resolve #28; Function ft_strmapi.c is okay

--- Libft/ft_strmapi.c
+++ Libft/ft_strmapi.c
@@ -6,8 +6,30 @@
 /*   By: lluciano <lluciano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 14:03:44 by lluciano          #+#    #+#             */
-/*   Updated: 2021/09/20 22:09:10 by lluciano         ###   ########.fr       */
+/*   Updated: 2021/09/29 15:25:30 by lluciano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	len;
+	size_t	i;
+	char	*str;
+
+	if (!s || !f)
+		return (NULL);
+	i = 0;
+	len = ft_strlen(s);
+	str = (char *)malloc (sizeof(char) * len + 1);
+	if (str == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
