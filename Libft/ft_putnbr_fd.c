a930f0bf00e9e56b988bfcd14bb544e52b865af4
Luan <luanldcarvalho@gmail.com>
2021-10-01 13:55:15 -0300

    resolve #33; ft_putnbr_fd.c is okay

--- Libft/ft_putnbr_fd.c
+++ Libft/ft_putnbr_fd.c
@@ -6,8 +6,28 @@
 /*   By: lluciano <lluciano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 14:05:09 by lluciano          #+#    #+#             */
-/*   Updated: 2021/09/16 14:09:07 by lluciano         ###   ########.fr       */
+/*   Updated: 2021/10/01 13:54:23 by lluciano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putnbr_fd(int n, int fd);
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	unsigned int	number;
+
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		number = n * -1;
+	}
+	else
+		number = n;
+	if (number >= 10)
+	{
+		ft_putnbr_fd(number / 10, fd);
+		ft_putnbr_fd(number % 10, fd);
+	}
+	else
+		ft_putchar_fd(number + 48, fd);
+}
