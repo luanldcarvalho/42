1d80ca8cae353360c65fd79d06f3eaaada481cb5
Luan <luanldcarvalho@gmail.com>
2021-10-12 18:22:20 -0300

    resolve #44; First commit GNL Project

---
+++ GNL/get_next_line_utils.c
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lluciano <lluciano@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/12 18:17:54 by lluciano          #+#    #+#             */
+/*   Updated: 2021/10/12 18:19:53 by lluciano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+You must add a get_next_line_utils.c file
+which will contain the functions that are needed for your get_next_line to work.
+*/
